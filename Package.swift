90bff66070be98e0f20f575c6c0f68d229444a03
Yo Sato <yosato16@gmail.com>
2026-07-07 11:11:05 +0900

    dependency on the google admob

--- Package.swift
+++ Package.swift
@@ -7,11 +7,15 @@ let package = Package(
     products: [
         .library(name: "AdMobUtilities", targets: ["AdMobUtilities"])
     ],
-    dependencies: [],
+    dependencies: [        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "13.6.0")
+],
     targets: [
         .target(
             name: "AdMobUtilities",
-            dependencies: []
+            dependencies: [
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+
+            ]
         )
     ]
 )
